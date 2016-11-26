ec32cbf00d72c76e1b77c7b4f8e44c953da7ade9
JinSeong <rlawlstjd077@gmail.com>
2016-11-27 06:20:20 +0900

    adding scanning function simple list

--- hearyouare/app/src/main/java/com/example/dsm_025/hearyouare/MainFragment.java
+++ hearyouare/app/src/main/java/com/example/dsm_025/hearyouare/MainFragment.java
@@ -1,9 +1,9 @@
 package com.example.dsm_025.hearyouare;
 
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v4.widget.Space;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -30,9 +30,13 @@ public class MainFragment extends Fragment {
         iv.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(getContext(),"버튼을 눌렀습니다.",Toast.LENGTH_LONG).show();
-                SpeakerScanFragment speakerScanFragment = SpeakerScanFragment.newInstance();
-                speakerScanFragment.show(((AppCompatActivity)getContext()).getSupportFragmentManager(), null);
+                WifiManager wifiManager = (WifiManager)getContext().getSystemService(getContext().WIFI_SERVICE);
+                if(wifiManager.isWifiEnabled()) {
+                    SpeakerScanDialogFragment speakerScanDialogFragment = SpeakerScanDialogFragment.newInstance();
+                    speakerScanDialogFragment.show(((AppCompatActivity) getContext()).getSupportFragmentManager(), null);
+                }else{
+                    Toast.makeText(getContext(), "Wifi가 꺼져 있습니다.", Toast.LENGTH_SHORT).show();
+                }
             }
         });
         return v;
--- hearyouare/app/src/main/java/com/example/dsm_025/hearyouare/ScanningWifi.java
+++ hearyouare/app/src/main/java/com/example/dsm_025/hearyouare/ScanningWifi.java
@@ -1,10 +1,14 @@
 package com.example.dsm_025.hearyouare;
 
 import android.content.Context;
+import android.content.BroadcastReceiver;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.wifi.ScanResult;
 import android.net.wifi.WifiManager;
+import android.util.Log;
 import android.widget.ImageButton;
 import android.widget.SimpleAdapter;
 import android.widget.Toast;
@@ -18,15 +22,49 @@ import java.util.List;
  */
 public class ScanningWifi {
 
+    private static final String TAG = "WIFIScanner";
     private WifiManager wifiManager;
     private ConnectivityManager connManager;
     private List<ScanResult> scanResult;
     private SimpleAdapter adapter;
     private ArrayList<HashMap<String,String>> list;
     private Context mContext;
+    private int scanCount = 0;
+    String result = "";
+    StringBuilder sb = new StringBuilder();
 
-    public void ScanningWifi(Context context){
+    private BroadcastReceiver mWifiScanReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            final String action = intent.getAction();
+            if(action.equals(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION)){
+                getWIFIScanResult();
+                wifiManager.startScan();
+            }else if(action.equals(WifiManager.NETWORK_STATE_CHANGED_ACTION)){
+                mContext.sendBroadcast(new Intent("wifi.ON_NETWORK_STATE_CHANGED"));
+            }
+        }
+    };
+    public void getWIFIScanResult(){
+        scanResult = wifiManager.getScanResults();
+        for (int i = 0; i < scanResult.size(); i++) {
+            ScanResult result = scanResult.get(i);
+            Log.d(TAG, ((i + 1) + ". SSID : " + result.SSID.toString() + "\t\t RSSI : " + result.level + " dBm\n"));
+        }
+    }
+    public void initWIFIScan(){
+        wifiManager = (WifiManager)mContext.getSystemService(Context.WIFI_SERVICE);
+        scanCount = 0;
+        final IntentFilter filter = new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION);
+        filter.addAction(WifiManager.NETWORK_STATE_CHANGED_ACTION);
+        mContext.registerReceiver(mWifiScanReceiver, filter);
+        wifiManager.startScan();
+    }
+
+
+    public ScanningWifi(Context context){
         mContext = context;
+        initWIFIScan();
     }
 
     public void getWifi(){
@@ -35,16 +73,21 @@ public class ScanningWifi {
         connManager = (ConnectivityManager)mContext.getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo wifiInfo = connManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
 
-        if(wifiInfo.isAvailable()){
-
+        if(wifiManager.isWifiEnabled()){
         }else{
-            Toast.makeText(mContext, "wifi 사용불가", Toast.LENGTH_SHORT).show();
-
+            Toast.makeText(mContext, "wifi 가 꺼져 있습니다.", Toast.LENGTH_SHORT).show();
+            return;
         }
 
+
+
+        mContext.registerReceiver(mWifiScanReceiver, new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
+        wifiManager.startScan();
     }
-    public void refresh(){
-        scanResult = wifiManager.getScanResults();
-        
+    public void startScan(){
+        wifiManager.startScan();
+    }
+    public void stopScan(){
+        mContext.unregisterReceiver(mWifiScanReceiver);
     }
 }
